9cf79ce9c9438db63247bbfa2bf600230dc7b2e2
satwikmishra1107 <117844867+satwikmishra1107@users.noreply.github.com>
2025-03-13 21:13:24 +0530

    server updates

--- server/app.js
+++ server/app.js
@@ -56,6 +56,6 @@ app.get("/api/retrieve/:fileHash", async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
