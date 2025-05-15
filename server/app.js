5524015e13dd91c906ac34e1f7ac835d29e78d7f
Pome <117844867+satwikmishra1107@users.noreply.github.com>
2025-05-16 01:06:55 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = process.env.PORT || 1234;
+const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
